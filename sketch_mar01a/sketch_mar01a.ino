320921c276d45cabb3cfd28a0769163d7fa586f9
Hugo Costa Machado <hugocm93@gmail.com>
2016-03-01 23:01:37 -0300

    ..

--- sketch_mar01a/sketch_mar01a.ino
+++ sketch_mar01a/sketch_mar01a.ino
@@ -1,27 +1,28 @@
-#define BUTTON 2
-#define LED 13
+#define BUTTON1 2
+#define BUTTON2 4
+#define LED 12
 
 bool flag = false;
 
 void setup() {
   // put your setup code here, to run once:
-  
-  pinMode(BUTTON, INPUT);
+  pinMode(BUTTON1, INPUT);
+  pinMode(BUTTON2, INPUT);
   pinMode(LED, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  if(digitalRead(BUTTON) == HIGH){
+  
+  if( digitalRead(BUTTON1) == LOW){
     flag = true;
   }
   
   if(flag == false){
-    digitalWrite(LED, HIGH);
-    delay(1000);
     digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
     delay(1000);
   }
 }
