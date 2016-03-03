e5a915206fc12da925ea2b46ec1ab7b3b3fc640d
Hugo Costa Machado <hugocm93@gmail.com>
2016-03-02 21:41:54 -0300

    Exercicio1 added

--- exercicio1/exercicio1.ino
+++ exercicio1/exercicio1.ino
@@ -1,24 +1,25 @@
 #define BUTTON1 2
-#define BUTTON2 4
 #define LED 12
 
+//Flag that controls the state of the button. False means it was never pressed.
 bool flag = false;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(BUTTON1, INPUT);
-  pinMode(BUTTON2, INPUT);
   pinMode(LED, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
+  //Checks if the button was pressed. 
   if( digitalRead(BUTTON1) == LOW){
     flag = true;
   }
-  
+
+  //Blinks LED if button was never pressed. Otherwise the button remains in the HIGH state.
   if(flag == false){
     digitalWrite(LED, LOW);
     delay(1000);
